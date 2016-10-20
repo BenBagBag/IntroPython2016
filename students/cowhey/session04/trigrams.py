efb9ad86a4a11806dba0eda04b754daa645d5960
BenBagBag <IsaacC@allenai.org>
2016-10-19 21:36:31 -0700

    upgrade trigram program to run with command line arguments

--- students/cowhey/session04/trigrams.py
+++ students/cowhey/session04/trigrams.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
+'''Writes a random text using trigrams from a supplied text.
+Use:
+$ python3 trigrams.py filename fileLength frontMatter endMatter
+filename: file name, including file path
+fileLength: an integer
+frontMatter: file's front matter end string (must be in quotation marks, optional)
+endMatter: file's end matter end string (must be in quotation marks, optional)'''
+
+
 import random
+from sys import argv
 
 
 def make_trigrams(word_list):
@@ -16,32 +26,33 @@ def make_trigrams(word_list):
     return trigrams
 
 
-def read_file(file_name):
+def read_file(file_name, front_matter, end_matter):
     '''Read an input file.
     Return a list of all words in the file.
     file_name: string of file name, including file path if not in current directory'''
     with open(file_name, "r") as input_file:
-        skip_front_matter(input_file)
+        if front_matter:
+            skip_front_matter(input_file, front_matter)
         all_words = []
         for line in input_file:
             # skip end of book
-            if line.startswith("End of the Project Gutenberg EBook"):
+            if end_matter and line.startswith(end_matter):
                 break
             else:
                 all_words += line.rstrip().split()
     return all_words
 
 
-def skip_front_matter(f):
+def skip_front_matter(f, front_matter):
     '''Skip over the front matter in the book
-    f: an open file object'''
-    end_front_matter = " XII. The Adventure of the Copper Beeches"
+    f: an open file object
+    front_matter: string of the front matter ending line'''
     for line in f:
-        if line.startswith(end_front_matter):
+        if line.startswith(front_matter):
             break
 
 
-def write_trigram_text(trigram_dict, length):
+def choose_trigrams(trigram_dict, length):
     '''Return a text produced from trigrams.
     trigram_dict: a dictionary of trigrams,
                 key is a bigram, value is a list of the third trigram element
@@ -55,11 +66,23 @@ def write_trigram_text(trigram_dict, length):
             next_word = random.choice(trigram_dict[last_bigram])
             output_words.append(next_word)
         else:
-            write_trigram_text(trigram_dict, n)
+            choose_trigrams(trigram_dict, n)
     return " ".join(output_words)
 
-if __name__ == "__main__":
-    all_words = read_file("holmes.txt")
+
+def write_trigram_text(input_file, length, front_matter=None, end_matter=None):
+    '''Print a text produced from trigrams derived from input file.
+    input_file: string of file name
+    front_matter: text of final line of text's front matter (title, publication info)
+    end_matter: text of final line of text's end matter (acknowledgments, etc.)
+    length: length, in words, of ouput text'''
+    all_words = read_file(input_file, front_matter, end_matter)
     trigrams = make_trigrams(all_words)
-    final_text = write_trigram_text(trigrams, 300)
+    length = int(length)
+    final_text = choose_trigrams(trigrams, length)
     print(final_text)
+
+
+if __name__ == "__main__":
+    args = argv[1:]
+    write_trigram_text(*args)
