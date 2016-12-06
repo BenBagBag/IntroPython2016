44c772b94cad5f31ade96110513498cbc76b22f7
BenBagBag <IsaacC@allenai.org>
2016-12-05 17:23:25 -0800

    front end works

--- students/cowhey/final-project/app.py
+++ students/cowhey/final-project/app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 import location
 app = Flask(__name__)
 
+test_weather = {'humidity': 93, 'temp': 37.4, 'rain': {'3h': 4}, 'status': 'light rain', 'wind': 1.5, 'clouds': 90, 'snow': {'3h': 5}}
+
 @app.route('/')
 def hello_world():
     city = location.get_location()
     weather = location.get_weather(city)
-    # current_condition = weather.get_detailed_status()
+    clothing = location.determine_clothing(weather)
     # feed a dictionary into this
-    return render_template("index.html", weather=weather)
+    return render_template("index.html", city=city, weather=weather, clothing=clothing)
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
