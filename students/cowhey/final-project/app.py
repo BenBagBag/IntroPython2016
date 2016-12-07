bb791d54ae74d39e16ed1ec61a952758a519c34d
BenBagBag <IsaacC@allenai.org>
2016-12-07 10:51:20 -0800

    refactor api call

--- students/cowhey/final-project/app.py
+++ students/cowhey/final-project/app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template
-import location
+from weather_data import WeatherData
 app = Flask(__name__)
 
 test_weather = {'humidity': 93, 'temp': 37.4, 'rain': {'3h': 4}, 'status': 'light rain', 'wind': 1.5, 'clouds': 90, 'snow': {'3h': 5}}
 
 @app.route('/')
 def hello_world():
-    city = location.get_location()
-    weather = location.get_weather(city)
-    clothing = location.determine_clothing(weather)
-    # feed a dictionary into this
-    return render_template("index.html", city=city, weather=weather, clothing=clothing)
+    data = WeatherData()
+    return render_template("index.html", city=data.location, weather=data.weather_dict, clothing=data.clothing)
 
 @app.route('/faq')
 def faq():
