dc874b5933cbb23424dd689e8a37fc1932e7a4f1
BenBagBag <IsaacC@allenai.org>
2016-12-10 17:20:42 -0800

    add tests

--- students/cowhey/final-project/app.py
+++ students/cowhey/final-project/app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template, request, url_for
 from weather_data import WeatherData
+
 app = Flask(__name__)
 
-test_weather = {'humidity': 93, 'temp': 37.4, 'rain': {'3h': 4}, 'status': 'light rain', 'wind': 1.5, 'clouds': 90, 'snow': {'3h': 5}}
 
 @app.route('/')
 def hello_world():
     data = WeatherData()
     return render_template("index.html", city=data.location, weather=data.weather_dict, clothing=data.clothing, searchCity=data.userProvidedLocation)
 
+
 @app.route('/faq')
 def faq():
     return render_template('faq.html', faq=True)
 
+
 @app.route('/search')
 def search():
     return render_template('search.html', searchCity=True)
+
 
 @app.route('/search', methods=['POST'])
 def searchcity():
